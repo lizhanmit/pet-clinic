a7ddf141d7fd98ee5953f4f5fc962acbf632004c
Zhan Li <zhan.li.dev@outlook.com>
2018-12-26 15:13:15 +1100

    Updated Vets index page and controller.

--- pet-clinic-web/src/main/java/com/zhandev/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/zhandev/petclinic/controller/VetController.java
@@ -3,10 +3,11 @@ package com.zhandev.petclinic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-	@RequestMapping({"/", "vets/index", "vets/index.html"})  // requesting any of these URLs can direct to the same vets index page
+	@RequestMapping({"", "/", "/index", "/index.html"})  // requesting any of these URLs can direct to the same vets index page
 	public String listVets() {
 		return "vets/index";  // "templates" folder -> "vets" folder -> "index.html" file
 	}
