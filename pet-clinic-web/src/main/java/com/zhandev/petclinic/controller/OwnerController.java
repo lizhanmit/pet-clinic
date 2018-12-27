592750ecb5a236f42bef6b85f742de0c76b83b31
Zhan Li <zhan.li.dev@outlook.com>
2018-12-28 09:24:59 +1100

    Added all owners on owner index page. Close #16.

--- pet-clinic-web/src/main/java/com/zhandev/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/zhandev/petclinic/controller/OwnerController.java
@@ -1,14 +1,24 @@
 package com.zhandev.petclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zhandev.petclinic.service.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+	private final OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}
+
 	@RequestMapping({"", "/", "/index", "/index.html"})  // requesting any of these URLs can direct to the same owners index page
-	public String listOwners() {
+	public String listOwners(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
 		return "owners/index";  // "templates" folder -> "owners" folder -> "index.html" file
 	}
 }
