1964ec1ae0cf2f6e841d16ffa8faf8713fa77a34
Zhan Li <zhan.li.dev@outlook.com>
2018-12-28 10:18:55 +1100

    Added all vets on vet index page. Close #17.

--- pet-clinic-web/src/main/java/com/zhandev/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/zhandev/petclinic/controller/VetController.java
@@ -1,14 +1,24 @@
 package com.zhandev.petclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zhandev.petclinic.service.VetService;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
 	@RequestMapping({"", "/", "/index", "/index.html"})  // requesting any of these URLs can direct to the same vets index page
-	public String listVets() {
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";  // "templates" folder -> "vets" folder -> "index.html" file
 	}
 }
