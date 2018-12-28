d51d4acb7fa5318ea8aa81e52122dadf04dd9b5e
Zhan Li <zhan.li.dev@outlook.com>
2018-12-28 11:20:34 +1100

    Refactored service map implementation to manage setting of Id property. Close #23.

--- pet-clinic-data/src/main/java/com/zhandev/petclinic/service/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/zhandev/petclinic/service/map/AbstractMapService.java
@@ -1,13 +1,18 @@
 package com.zhandev.petclinic.service.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
+import com.zhandev.petclinic.model.BaseEntity;
 
-	protected Map<ID, T> map = new HashMap<>();
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+	protected Map<Long, T> map = new HashMap<>();
 	
 	Set<T> findAll() {
 		return new HashSet<>(map.values());
@@ -17,11 +22,32 @@ public abstract class AbstractMapService<T, ID> {
 		return map.get(id);
 	}
 	
-	T save(ID id, T object) {
-		map.put(id, object);
+	T save(T object) {
+		
+		if (object != null) {
+			if (object.getId() == null) {
+				object.setId(getNextId());
+			}
+			
+			map.put(object.getId(), object);
+		} else {
+			throw new RuntimeException("Object cannot be null.");
+		}
+		
 		return object;
 	}
 	
+	private Long getNextId() {
+		Long maxId = null;
+		
+		try {
+			maxId = Collections.max(map.keySet()) + 1;
+		} catch (NoSuchElementException e) {
+			maxId = 1L;
+		}
+		return maxId;
+	}
+
 	void delete(T object) {
 		map.entrySet().removeIf(entry -> entry.getValue().equals(object));
 	}
