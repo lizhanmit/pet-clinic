950d9f0fa9ad00da26e82dbacfa436ca2e9c6504
Zhan Li <zhan.li.dev@outlook.com>
2018-12-30 17:11:49 +1100

    Added contact info bootstrap data for owners and petType data for pets. Close #30.

--- pet-clinic-web/src/main/java/com/zhandev/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/zhandev/petclinic/bootstrap/DataLoader.java
@@ -1,13 +1,17 @@
 package com.zhandev.petclinic.bootstrap;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.zhandev.petclinic.model.Owner;
+import com.zhandev.petclinic.model.Pet;
 import com.zhandev.petclinic.model.PetType;
 import com.zhandev.petclinic.model.Vet;
 import com.zhandev.petclinic.service.OwnerService;
+import com.zhandev.petclinic.service.PetService;
 import com.zhandev.petclinic.service.PetTypeService;
 import com.zhandev.petclinic.service.VetService;
 
@@ -16,14 +20,16 @@ public class DataLoader implements CommandLineRunner {
 
 	private final OwnerService ownerService;
 	private final VetService vetService;
+	private final PetService petService;
 	private final PetTypeService petTypeService;
 
 	// apply DI
 	// if you have only one constructor, it is not necessary to annotate "@Autowired" here
 	@Autowired
-	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+	public DataLoader(OwnerService ownerService, VetService vetService, PetService petService, PetTypeService petTypeService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petService = petService;
 		this.petTypeService = petTypeService;
 	}
 
@@ -40,12 +46,36 @@ public class DataLoader implements CommandLineRunner {
 		Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("1231231234");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        Pet savedMikesPet = petService.save(mikesPet);
         
+        owner1.getPets().add(savedMikesPet);
+
         ownerService.save(owner1);
         
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("123 Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("1231231234");
+        
+        Pet fionasCat = new Pet();
+        fionasCat.setName("Just Cat");
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setPetType(savedCatPetType);
+        Pet savedFionasCat = petService.save(fionasCat);
+        
+        owner2.getPets().add(fionasCat);
         
         ownerService.save(owner2);
         
