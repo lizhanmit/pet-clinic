0a33a6556782ff98c48f04971ca8e49ca723e8b5
Zhan Li <zhan.li.dev@outlook.com>
2018-12-30 17:31:42 +1100

    Added Specialty bootstrap data. Close #32.

--- pet-clinic-web/src/main/java/com/zhandev/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/zhandev/petclinic/bootstrap/DataLoader.java
@@ -9,10 +9,12 @@ import org.springframework.stereotype.Component;
 import com.zhandev.petclinic.model.Owner;
 import com.zhandev.petclinic.model.Pet;
 import com.zhandev.petclinic.model.PetType;
+import com.zhandev.petclinic.model.Specialty;
 import com.zhandev.petclinic.model.Vet;
 import com.zhandev.petclinic.service.OwnerService;
 import com.zhandev.petclinic.service.PetService;
 import com.zhandev.petclinic.service.PetTypeService;
+import com.zhandev.petclinic.service.SpecialtyService;
 import com.zhandev.petclinic.service.VetService;
 
 @Component
@@ -22,19 +24,26 @@ public class DataLoader implements CommandLineRunner {
 	private final VetService vetService;
 	private final PetService petService;
 	private final PetTypeService petTypeService;
+	private final SpecialtyService specialtyService;
 
 	// apply DI
 	// if you have only one constructor, it is not necessary to annotate "@Autowired" here
 	@Autowired
-	public DataLoader(OwnerService ownerService, VetService vetService, PetService petService, PetTypeService petTypeService) {
+	public DataLoader(OwnerService ownerService, VetService vetService, PetService petService, PetTypeService petTypeService, SpecialtyService specialtyService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
 		this.petService = petService;
 		this.petTypeService = petTypeService;
+		this.specialtyService = specialtyService;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		initData();
+		
+	}
+
+	private void initData() {
 		PetType dog = new PetType();
         dog.setName("Dog");
         PetType savedDogPetType = petTypeService.save(dog);
@@ -56,6 +65,18 @@ public class DataLoader implements CommandLineRunner {
         mikesPet.setBirthDate(LocalDate.now());
         mikesPet.setName("Rosco");
         Pet savedMikesPet = petService.save(mikesPet);
+        
+        Specialty radiology = new Specialty();
+        radiology.setDescription("Radiology");
+        Specialty savedRadiology = specialtyService.save(radiology);
+
+        Specialty surgery = new Specialty();
+        surgery.setDescription("Surgery");
+        Specialty savedSurgery = specialtyService.save(surgery);
+
+        Specialty dentistry = new Specialty();
+        dentistry.setDescription("dentistry");
+        Specialty savedDentistry = specialtyService.save(dentistry);
         
         owner1.getPets().add(savedMikesPet);
 
@@ -84,17 +105,18 @@ public class DataLoader implements CommandLineRunner {
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
+        vet1.getSpecialties().add(savedRadiology);
         
         vetService.save(vet1);
         
         Vet vet2 = new Vet();
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
+        vet2.getSpecialties().add(savedSurgery);
         
         vetService.save(vet2);
         
         System.out.println("Loaded vets ...");
-		
 	}
 
 }
